dc708e4ea13edf271836ea077b9bb2e9da9aa874
suzi01 <suzannahthompson@hotmail.co.uk>
2022-10-30 18:43:47 +0000

    Additional commit to check test parameter for timeline items

--- js/index.js
+++ js/index.js
@@ -14,9 +14,9 @@ function makeTimeline(timelines){
     headerEl.textContent = 'Timeline'
     background.appendChild(headerEl)
 
-    const timelineItemsEL = document.createElement('div')
-    timelineItemsEL.classList.add('timeline-items')
-    background.appendChild(timelineItemsEL)
+    // const timelineItemsEL = document.createElement('div')
+    // timelineItemsEL.classList.add('timeline-items')
+    // background.appendChild(timelineItemsEL)
 
    
 
@@ -36,78 +36,12 @@ function makeTimeline(timelines){
                     <button class="timeline-item-more-info" id="showButton">Read More</button>
                 </div>
         `
-        modalElem.innerHTML= `
-           
-            <div class="modal-header">
-                <button id="hideIcon">
-                    <i class="fa fa-close"></i>
-                </button>
-            </div>
-            <div class="modal-body">
-                <h1 class="modal-title">${title}<h1>
-                <img class="modal-image" src=${image} alt="...">
-                <h3 id="model-date">${date}</h3>
-                <p class="modal-full-description">
-                    ${fullDescription}
-                </p>
-                <button class="modal-close-button" id="hideButton">
-                    close
-                </button>
-            </div>
-        `
-
-        timelineItemsEL.appendChild(dateEl)
-        document.body.appendChild(modalElem)
+        background.appendChild(dateEl)
     })
 
 }
 
 
-var modalMulti = document.getElementsByClassName("modal-multi");
-var showButtons = document.getElementsByClassName("timeline-item-more-info");
-var closeButton = document.getElementsByClassName("modal-close-button");
-
-console.log(closeButton)
-
-
-function setDataIndex() {
-    for (let i=0; i<showButtons.length; i++){
-        showButtons[i].setAttribute('data-index',i)
-        modalMulti[i].setAttribute('data-index', i)
-        closeButton[i].setAttribute('data-index', i)
-    }
-}
-
-
-for (let j = 0; j < showButtons.length; j++)
-    {
-        showButtons[j].onclick = function() {
-            var ElementIndex = this.getAttribute('data-index');
-            modalMulti[ElementIndex].id = 'show';
-            background.classList.add('blur')
-            console.log(j)
-        };
-
-        // When the user clicks on <span> (x), close the modal
-        closeButton[j].onclick = function() {
-            var ElementIndex = this.getAttribute('data-index');
-            modalMulti[ElementIndex].id = "hide";
-            background.classList.remove('blur')
-            console.log('closing',j)
-        };
-
-    }
-
-window.onload = function() {
-    setDataIndex();
-};
-
-window.onclick = function(event) {
-    if (event.target === modalMulti[event.target.getAttribute('data-index')]) {
-        modalMulti[event.target.getAttribute('data-index')].id = "hide";
-        // console.log('here!!!')
-    }
-};
 
 
 
